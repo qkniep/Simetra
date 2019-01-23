3ef369af864bf9e552662c9366d418316678ba43
Quentin Kniep <hello@quentinkniep.com>
2019-01-23 17:06:45 +0100

    fixed memory error after deleting old chunks

--- src/world.cpp
+++ src/world.cpp
@@ -17,20 +17,13 @@ World::World(int s) : seed(s), allChunks(4 * 7, NULL), deletePtr(21), currentRow
 	allChunks[9]  = new Chunk( 1, 2);
 	allChunks[4]  = new Chunk( 2, 2);
 
-	/*allChunks[(deletePtr - 3 * 7) % allChunks.size() + 5] = new Chunk(-3, currentRow);
+	allChunks[(deletePtr - 3 * 7) % allChunks.size() + 5] = new Chunk(-3, currentRow);
 	allChunks[(deletePtr - 2 * 7) % allChunks.size() + 3] = new Chunk(-2, currentRow);
 	allChunks[(deletePtr - 1 * 7) % allChunks.size() + 1] = new Chunk(-1, currentRow);
 	allChunks[deletePtr]                                  = new Chunk( 0, currentRow);
 	allChunks[(deletePtr - 1 * 7) % allChunks.size() + 2] = new Chunk( 1, currentRow);
 	allChunks[(deletePtr - 2 * 7) % allChunks.size() + 4] = new Chunk( 2, currentRow);
-	allChunks[(deletePtr - 3 * 7) % allChunks.size() + 6] = new Chunk( 3, currentRow);*/
-	allChunks[5]  = new Chunk(-3, 3);
-	allChunks[10] = new Chunk(-2, 3);
-	allChunks[15] = new Chunk(-1, 3);
-	allChunks[21] = new Chunk( 0, 3);
-	allChunks[16] = new Chunk( 1, 3);
-	allChunks[11] = new Chunk( 2, 3);
-	allChunks[6]  = new Chunk( 3, 3);
+	allChunks[(deletePtr - 3 * 7) % allChunks.size() + 6] = new Chunk( 3, currentRow);
 	deletePtr = 0;
 }
 
@@ -55,6 +48,7 @@ void World::loadNextRow() {
 	for (int i = deletePtr; i < deletePtr + 7; ++i) {
 		if (allChunks[i] == NULL) continue;
 		delete allChunks[i];
+		allChunks[i] = 0;
 	}
 
 	currentRow += 1;
@@ -65,13 +59,6 @@ void World::loadNextRow() {
 	allChunks[(deletePtr + 3 * 7) % allChunks.size() + 2] = new Chunk( 1, currentRow);
 	allChunks[(deletePtr + 2 * 7) % allChunks.size() + 4] = new Chunk( 2, currentRow);
 	allChunks[(deletePtr + 1 * 7) % allChunks.size() + 6] = new Chunk( 3, currentRow);
-	std::cout << "i: " << (deletePtr + 1 * 7) % allChunks.size() + 5 << std::endl;
-	std::cout << "i: " << (deletePtr + 2 * 7) % allChunks.size() + 3 << std::endl;
-	std::cout << "i: " << (deletePtr + 3 * 7) % allChunks.size() + 1 << std::endl;
-	std::cout << "i: " << deletePtr << std::endl;
-	std::cout << "i: " << (deletePtr + 3 * 7) % allChunks.size() + 2 << std::endl;
-	std::cout << "i: " << (deletePtr + 2 * 7) % allChunks.size() + 4 << std::endl;
-	std::cout << "i: " << (deletePtr + 1 * 7) % allChunks.size() + 6 << std::endl;
 	deletePtr += 7;
 	deletePtr %= allChunks.size();
 
